8d178171244416c3d00bad46ff32b20abf2f6ad3
Shoham <180756250+shohamdeb@users.noreply.github.com>
2024-12-06 02:03:02 +0000

    Day 5 🎆

---
+++ 2024/5.py
@@ -0,0 +1,38 @@
+from collections import defaultdict as dd
+from functools import cmp_to_key
+
+reached_split = False
+rules = dd(set)
+
+def cmp(x, y):
+    return -1 if x in rules[y] else 0
+
+total_a = 0
+total_b = 0
+
+r_data, u_data = open("inputs/5.txt").read().split("\n\n")
+
+for line in r_data.split():
+    x, y = map(int, line.split("|"))
+    rules[y].add(x) # backwards
+
+for line in u_data.split():
+    banned = set()
+    update = [*map(int, line.split(","))]
+    crap = False
+
+    for n in update:
+        if n in banned:
+            crap = True
+            break
+        else:
+            banned |= rules[n]
+
+    if not crap:
+        total_a += update[len(update) // 2]
+    else:
+        sorted_update = sorted(update, key = cmp_to_key(cmp))
+        total_b += sorted_update[len(sorted_update) // 2]
+
+print("Part A:", total_a)
+print("Part B:", total_b)
